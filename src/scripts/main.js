ee7f1a45c573c4c7f5c6f508f4e0e535f0a46e80
Mukola <vmv.vynnychuk98@gmail.com>
2025-11-04 19:47:07 +0200

    header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+const swiper = new Swiper('.header__swiper', {
+  loop: true,
+  direction: 'horizontal',
+  slidesPerView: 1,
+  rtl:true,
+  autoplay: {
+    delay: 190000000,
+  },
+  navigation: {
+    prevEl: '.swiper-button-next', 
+    nextEl: '.swiper-button-prev',
+  },
+});
