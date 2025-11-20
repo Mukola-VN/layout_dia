7bcdeca04b5231ef569319e1fe4caf563f9d0bc5
Mukola <vmv.vynnychuk98@gmail.com>
2025-11-20 18:58:59 +0200

    aside , header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,3 +10,16 @@ const swiper = new Swiper('.header__swiper', {
     nextEl: '.swiper-button-prev',
   },
 });
+
+const menu = document.getElementById("menu");
+const openBtn = document.querySelector(".icon--menu");
+const closeBtn = document.querySelector(".icon__close");
+
+openBtn.addEventListener("click", () => {
+  menu.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
+
